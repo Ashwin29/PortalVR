06c000cf414a5271b44a785a0d9f27f385a6f5b7
Ashwin <aashwin.win17@gmail.com>
2018-05-12 22:24:00 +0530

    Initial commit

---
+++ Assets/Scripts/InterdimensionalTransport.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class InterdimensionalTransport : MonoBehaviour {
+
+    public Material[] materials;
+
+	// Use this for initialization
+	void Start () {
+        foreach (var mat in materials)
+        {
+            mat.SetInt("_StencilTest", (int)CompareFunction.Equal);
+        }
+    }
+
+    void OnTriggerStay(Collider other) {
+        if (other.name != "Main Camera") {
+            return;
+        }
+
+        //Outside of other world
+        if (transform.position.z > other.transform.position.z) {
+            Debug.Log("Outside of other World!!");
+            foreach (var mat in materials) {
+                mat.SetInt("_StencilTest", (int)CompareFunction.Equal);
+            }
+            
+        }
+        else
+        {
+            Debug.Log("Inside of other World!!");
+            foreach (var mat in materials) {
+                mat.SetInt("_StencilTest", (int)CompareFunction.NotEqual);
+            }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        foreach (var mat in materials)
+        {
+            mat.SetInt("_StencilTest", (int)CompareFunction.NotEqual);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Shaders/StencilFit.shader
@@ -0,0 +1,21 @@
+﻿Shader "Custom/StencilFit"
+{
+	Properties
+	{
+		_Color("Color", color) = ( 1, 1, 1, 1)
+	    [Enum(Equal, 3, NotEqual, 6)] _StencilTest("Stencil Test", int) = 6
+	}
+	SubShader
+	{
+		Color [_Color]
+		Stencil {
+		Ref 1
+		Comp [_StencilTest]
+	}
+
+		Pass
+		{
+			
+		}
+	}
+}
